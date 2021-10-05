c17d54649796b56989fdf92a4ee1d28986b6879b
Kay Patzwald <kay.patzwald@posteo.de>
2021-10-05 09:30:03 +0200

    Added i18n for settings page

--- settings/index.jsx
+++ settings/index.jsx
@@ -1,8 +1,9 @@
-import { STATIONS } from "../common/globals.js"
+//import { STATIONS } from "../common/globals.js"
+import { gettext } from "i18n";
 
-console.log("Opening BART Settings page");
+console.log("Opening BBT Settings page");
 
-let autoValues = [];
+/*let autoValues = [];
 for (let key in STATIONS) {
   autoValues.push( {
     "name": STATIONS[key] + " Northbound",
@@ -12,27 +13,32 @@ for (let key in STATIONS) {
     "name": STATIONS[key] + " Southbound",
     "value": { code: key, direction: "s" }
   } );
-}
+}*/
 
+// TODO Add i18n
+// TODO Eingabe validieren?
+// TODO Settings testen, wenn online
+
+/* Define the settings page */
 function mySettings(props) {
   return (
     <Page>
       <Section
-        title={<Text bold align="center">Bart Schedule</Text>}>
+        title={<Text bold align="center">BerlinBrandenburgTransport</Text>}>
         <AdditiveList
-          title="Select your favorite stations"
+          title={gettext("fav_station")}
           settingsKey="favorite_station_setting"
           maxItems="5"
           addAction={
             <TextInput
-              title="Add a Bart Station"
-              label="Name"
-              placeholder="Type something"
-              action="Add Station"
-              onAutocomplete={(value) => {
-                return autoValues.filter((option) =>
-                  option.name.toLowerCase().startsWith(value.toLowerCase()));
-              }}
+              title={gettext("add_number")}
+              label={gettext("station_number")}
+              placeholder={gettext("insert_number")}
+              action={gettext("add_number_action")}
+              //onAutocomplete={(value) => {
+              //  return autoValues.filter((option) =>
+              //    option.name.toLowerCase().startsWith(value.toLowerCase()));
+              //}}
             />
           }
         />
