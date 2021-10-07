7b526024971a62dd47ef7064e2e8582ef98f195b
Kay Patzwald <kay.patzwald@posteo.de>
2021-10-07 08:31:09 +0200

    Small tweaks for better results

--- companion/bvg.js
+++ companion/bvg.js
@@ -1,33 +1,23 @@
-// Not necessary for bvg-api
-/*export function BVGAPI(apiKey) {
-  if (apiKey !== undefined) {
-    this.apiKey = apiKey;
-  }
-  else {
-    // Default key for open public access.
-    this.apiKey = "MW9S-E7SL-26DU-VV8V";
-  }
-};*/
-
 export function BVGAPI() {
   
 };
 
 BVGAPI.prototype.realTimeDepartures = function(station_id) {
   let self = this;
+  console.log("Station ID: " + station_id);
   return new Promise(function(resolve, reject) {
     /*let url = "https://api.bart.gov/api/etd.aspx?json=y";
     url += "&key=" + self.apiKey;
     url += "&cmd=etd";
     url += "&orig=" + origin;*/
 
-    let url = "https://v5.bvg.transport.rest/stops/";
+    let url = "https://v5.vbb.transport.rest/stops/";
 //    url += 900000080202 + "/departures?duration=1"; //ID ist vom U Grenzallee
     // url += 900100003 + "/departures?duration=1"; //ID ist vom U Alexanderplatz
-    url += station_id + "/departures?duration=1";
+    url += station_id + "/departures?duration=10"; //Show departures for how many minutes?
 
     url += "&when=" + "now"; // actual time
-    url += "&results=4"; // only the first 4 results // TODO eingeschränkt, weil zu viele Ergebnisse
+    url += "&results=6"; // only the first 6 results // TODO eingeschränkt, weil zu viele Ergebnisse
     //console.log("URL: " + url);
     
     fetch(url).then(function(response) {
@@ -35,11 +25,17 @@ BVGAPI.prototype.realTimeDepartures = function(station_id) {
     }).then(function(json) {
       //console.log("Got JSON response from server:" + JSON.stringify(json));
 
-      let data = json;
+      //let data = json;
       
       let departures = [];
 
       // TODO Station muss nicht für alle Einträge mitgeliefert werden. Spart evtl. Speicherplatz
+
+      // if (typeof(json) == "object") {
+      //   console.log("bvg.js:realTimeDepartures(): No results!")
+      // } else {
+
+      console.log("Objekt-Typ: " + json.length);
 
       json.forEach( (trip) => {
         let d = {
@@ -50,27 +46,15 @@ BVGAPI.prototype.realTimeDepartures = function(station_id) {
           "station": trip["stop"]["name"]
         }
         departures.push(d);
-
-        /*trip["estimate"].forEach( (train) => {
-          let d = {
-            "to": destination["abbreviation"],
-            "minutes": Number.parseInt(train["minutes"]),
-            "platform": train["platform"],
-            "bike": (train["bikeflag"] === "1" ? true : false)
-          };
-          if (!Number.isInteger(d["minutes"])) {
-            d["minutes"] = 0;
-          }
-          departures.push(d);
-        });*/
       });
+    // }
 
-      // Sort departures (TODO)
+      // Sort departures (notwendig?)
       //departures.sort( (a,b) => { return (a["minutes"] - b["minutes"]) } );
 
       resolve(departures);
     }).catch(function (error) {
-      console.log("Error in bvg.js->realTimeDepartures()"); console.log(error)
+      console.log("Error in bvg.js->realTimeDepartures(): " + error);
       reject(error);
     });
   });
